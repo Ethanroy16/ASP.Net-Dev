3e2e4ecd3e9bbbbc864728e06a98f63b3b3bd853
Ethan Curran <L00181476@atu.ie>
2026-02-04 22:09:39 +0000

    Week3_Tut

---
+++ RP1.Services/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using RP1.DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RP1.Services
+{
+    public interface IUnitOfWork :IDisposable
+    {
+        IBrandRepo BrandRepo { get; }
+        void Save();
+    }
+}
